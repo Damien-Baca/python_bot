1e53ae817102759b8702401b420ff2d7dad0b283
unknown <damienbacadev@gmail.com>
2025-06-30 16:04:01 -0600

    Renaming main.py to bot.py. Adding on_member_join, on_message, and on_ready event handlers

---
+++ bot.py
@@ -0,0 +1,50 @@
+# bot.py
+import os
+import random
+
+import discord
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+
+client_intents = discord.Intents.all()
+client = discord.Client(intents=client_intents)
+
+
+@client.event
+async def on_ready():
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    print(f'{client.user} has connected to {guild.name}!')
+
+    members = '\n - '.join([member.name for member in guild.members])
+    print(f'Guild Members:\n - {members}\n')
+
+
+@client.event
+async def on_member_join(member):
+    await member.create_dm()
+    await member.dm_channel.send(f'Hi {member.name}, welcome to the server! Follow the rules or be DELETED!')
+
+
+@client.event
+async def on_message(message):
+    cyberman_quotes = [
+        'DELETE! DELETE! DELETE!',
+        'Emotions have tormented you all of your life. Now you will be set free. This is your liberation.',
+        'I\'m cold. I\'m so cold...',
+        'Destroy them! Destroy them at once!',
+        'We have freedom from disease, protection against heat and cold, true mastery.',
+        'Every empire has its time, and every empire falls. But that which is dead can live again... In the hands of a believer!'
+    ]
+    if message.author == client.user:
+        return
+
+    if message.content == 'cyberman!':
+        response = random.choice(cyberman_quotes)
+        await message.channel.send(response)
+
+
+client.run(TOKEN)
