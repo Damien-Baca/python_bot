4bcbc4a96e76a525f99a22cd2622bdfdfb83340f
unknown <damienbacadev@gmail.com>
2025-06-30 22:35:20 -0600

    Add functionality for screenshot channel

--- bot.py
+++ bot.py
@@ -1,11 +1,22 @@
 # bot.py
+from sys import stderr, exit
 from os import getenv
 from random import choice
 
-from discord import Intents
+from discord import Intents, utils, DMChannel
 from discord.ext.commands import Bot
 from dotenv import load_dotenv
 
+DEBUG = False
+SCREENSHOT_CHANNEL_WARNING = 'Messages in the screenshots channel must contain an attached image. You\'re message has been DELETED!'
+CYBERMAN_QUOTES = [
+    'DELETE! DELETE! DELETE!',
+    'Emotions have tormented you all of your life. Now you will be set free. This is your liberation.',
+    'I\'m cold. I\'m so cold...',
+    'Destroy them! Destroy them at once!',
+    'We have freedom from disease, protection against heat and cold, true mastery.',
+    'Every empire has its time, and every empire falls. But that which is dead can live again... In the hands of a believer!'
+]
 
 load_dotenv(override=True)
 TOKEN = getenv('DISCORD_TOKEN')
@@ -17,21 +28,34 @@ bot = Bot(command_prefix='!', intents=bot_intents)
 
 @bot.event
 async def on_ready():
-    if bot.user:
-        print(f'{bot.user.name} has connected to {GUILD}!')
+    global guild
+    guild = utils.get(bot.guilds, name=GUILD)
+    if not guild:
+        print(f'Bot is not connected to a Guild. Exiting!', file=stderr)
+        exit()
+
+    if bot.user and guild:
+        print(f'{bot.user.name} has connected to {guild.name}!')
+        members = '\n - '.join([member.name for member in guild.members])
+        print(f'Guild Members:\n - {members}')
+
+
+@bot.event
+async def on_message(message):
+    if type(message.channel) != DMChannel and message.channel.name == 'screenshots':
+        if not message.attachments and guild:
+            await message.delete()
+            member = utils.get(guild.members, name=message.author.name)
+            if member:
+                dm = await member.create_dm()
+                await dm.send(SCREENSHOT_CHANNEL_WARNING)
+    else:
+        await bot.process_commands(message)
 
 
 @bot.command(name='cyberman', help='Prints a Cyber-man quote')
 async def quote_cybermen(ctx):
-    cyberman_quotes = [
-        'DELETE! DELETE! DELETE!',
-        'Emotions have tormented you all of your life. Now you will be set free. This is your liberation.',
-        'I\'m cold. I\'m so cold...',
-        'Destroy them! Destroy them at once!',
-        'We have freedom from disease, protection against heat and cold, true mastery.',
-        'Every empire has its time, and every empire falls. But that which is dead can live again... In the hands of a believer!'
-    ]
-    response = choice(cyberman_quotes)
+    response = choice(CYBERMAN_QUOTES)
     await ctx.send(response)
 
 
@@ -47,3 +71,5 @@ async def roll_dice(cxt, number_of_dice: int, number_of_sides: int):
 
 if TOKEN:
     bot.run(TOKEN)
+else:
+    print('Error retrieving DISCORD_TOKEN. Failed to start Bot.', file=stderr)
